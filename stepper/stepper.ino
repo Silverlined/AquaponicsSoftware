7228fddb02a8cabfe840aad15b33260f6c2405ce
Silverlined <dimitriy.georgiev@gmail.com>
2020-02-13 08:50:08 +0100

    Refactoring

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -8,18 +8,16 @@
 #define sm2_StepsPin 6
 #define sm3_DirectionPin 5
 #define sm3_StepsPin 4
+const int STEPS_PER_REVOLUTION = 1600;
 
 #define dcm_input1 10    //Input pins of the L298N to control DIRECTION of the motor
 #define dcm_input2 11
 #define servo_pin 12
-
-const int STEPS_PER_REVOLUTION = 1600;
 Servo servo;
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 uint16_t r, g, b, c;
 
-const int STEPS_PER_REVOLUTION = 1600;
 char buffer[32];
 const char EOL = '\n';    //command terminator (end of line)
 const char separator = ' ';
@@ -36,32 +34,33 @@ void takeColour(void) {
   Serial.println(" ");
 }
 
-int getNitrate(void) {  // Calculates the concentration of Nitrate (mg/L) based on carried out calibration.
-  return (int) (368 - 35 * log(b));
+float getNitrate(void) {  // Calculates the concentration of Nitrate (mg/L) based on carried out calibration.
+  return (2706 * exp(-0.00038 * b));
 }
 
-void startNitrateAssay(void) {
-  lengthenArm();
-  delay((int) (getActiveTime(4) * 1000));
-  stopArm();
-  //First Titrator
-  rotate(5, sm1_DirectionPin, sm1_StepsPin);
+// Not Finished
+// void startNitrateAssay(void) {
+//   lengthenArm();
+//   delay((int) (getActiveTime(4) * 1000));
+//   stopArm();
+//   //First Titrator
+//   rotate(5, sm1_DirectionPin, sm1_StepsPin);
 
-  lengthenArm();
-  delay((int) (getActiveTime(4) * 1000));
-  stopArm();
-  //Second Titrator
-  rotate(5, sm2_DirectionPin, sm2_StepsPin);
+//   lengthenArm();
+//   delay((int) (getActiveTime(4) * 1000));
+//   stopArm();
+//   //Second Titrator
+//   rotate(5, sm2_DirectionPin, sm2_StepsPin);
 
-  lengthenArm();
-  delay((int) (getActiveTime(17) * 1000));
-  stopArm();
-  //Colorimeter
-  //delay(3000 * 60);
-  takeColour();
-  Serial.println("Nitrate assay has finished:");
-  Serial.print("The concentration is: ");
-}
+//   lengthenArm();
+//   delay((int) (getActiveTime(17) * 1000));
+//   stopArm();
+//   //Colorimeter
+//   //delay(3000 * 60);
+//   takeColour();
+//   Serial.println("Nitrate assay has finished:");
+//   Serial.print("The concentration is: ");
+// }
 
 void initColourSensor(void) {
   if (tcs.begin()) {
@@ -77,7 +76,7 @@ void parseSerial(void) {
   String _read = String(buffer);    //Use Strings to make character processing easier.
   memset(buffer, 0, sizeof(buffer));    //clear, empty buffer
 
-  // Select sensor from associated data
+  // Select actuator from associated data
   int index = _read.indexOf(separator);
   cmd = _read.charAt(0);
 
@@ -125,22 +124,44 @@ void getSerial(void) {
         delay((int) (getActiveTime(value) * 1000));
         stopArm(); break;
       case '1':
-        rotate(value, sm1_DirectionPin, sm1_StepsPin);
+        rotate(getTurns(value), sm1_DirectionPin, sm1_StepsPin);
         break;
       case '2':
-        rotate(value, sm2_DirectionPin, sm2_StepsPin);
+        rotate(getTurns(value), sm2_DirectionPin, sm2_StepsPin);
         break;
       case '3':
-        rotate(value, sm3_DirectionPin, sm3_StepsPin);
+        rotate(getTurns(value), sm3_DirectionPin, sm3_StepsPin);
         break;
       case '4':
         servo.write(value);
         break;
       case '5':
         takeColour();
+        Serial.print("Nitrate Concentration (mg/L): ");
+        Seria.prinln(getNitrate());
         break;
     }
   }
+}
+
+void rotate(int turns, byte directionPin, byte stepsPin) {
+  int direction;
+  int steps = turns * STEPS_PER_REVOLUTION; 
+  if (steps > 0) {
+    direction = HIGH;
+  } else {
+    direction = LOW;
+  }
+  steps = abs(steps);
+  digitalWrite(directionPin, direction);
+  /*Steppin'*/
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(stepsPin, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(stepsPin, LOW);
+    delayMicroseconds(200);
+  }
+  delay(500);
 }
 
 void setup() {
@@ -160,6 +181,7 @@ void setup() {
   digitalWrite(sm2_StepsPin, LOW);
   digitalWrite(sm3_DirectionPin, LOW);
   digitalWrite(sm3_StepsPin, LOW);
+
   initColourSensor(); 
 
   servo.attach(servo_pin);
@@ -169,27 +191,5 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< HEAD
   getSerial();
-}
-
-void rotate(int turns, byte directionPin, byte stepsPin) {
->>>>>>> beff1471217ade1621863260e4f13d1c57a06b20
-  int direction;
-  int steps = turns * STEPS_PER_REVOLUTION; 
-  if (steps > 0) {
-    direction = HIGH;
-  } else {
-    direction = LOW;
-  }
-  steps = abs(steps);
-  digitalWrite(directionPin, direction);
-  /*Steppin'*/
-  for (int i = 0; i < steps; i++) {
-    digitalWrite(stepsPin, HIGH);
-    delayMicroseconds(200);
-    digitalWrite(stepsPin, LOW);
-    delayMicroseconds(200);
-  }
-  delay(500);
 }
